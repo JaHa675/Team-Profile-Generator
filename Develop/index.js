a0c17723b12bb5341a86d9cb5f377445b6e12d1d
James Harding <99635678+JaHa675@users.noreply.github.com>
2022-04-26 18:46:56 -0700

    added real readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
-const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
